053571b46eed6e9acc0386ffb45ade63de8fc26e
Yogesh <yogesh.singh.2810@gmail.com>
2018-04-21 17:51:08 +0530

    added redux file

--- redux/src/index.js
+++ redux/src/index.js
@@ -4,5 +4,21 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { createStore } from 'redux'
+import reducer from './reducer'
+
+const store = createStore(reducer)
+
+store.dispatch({
+  type : 'INCREMENT',
+  jump : 5
+})
+
+
+// store.getState()
+// store.dispatch()
+// store.subscribe()
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
